5691238a05ac2b81252ac0b7e5f664c2de757737
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-21 11:21:55 +0530

    Add new task component created for adding task

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddTask() {
+    return (
+        <div className='my-[20px]'>
+            <button className="btn btn-info w-[300px]">Add new Task</button>
+        </div>
+    )
+}
+
+export default AddTask
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import AddTask from "./components/AddTask";
 
 export default function Home() {
   return (
-    <main>
-      <h1>TODO APP!</h1>
+    <main className="flex flex-col justify-center items-center my-8">
+      <h1 className=" font-semibold text-[#fbfafaee] text-[20px]">TODO APP!</h1>
+      <AddTask />
     </main>
   );
 }
