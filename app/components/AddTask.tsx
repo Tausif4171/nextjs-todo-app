2c566e8bb41959c901957e294656aa87d081d280
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-21 11:41:14 +0530

    react icons added

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { CiCirclePlus } from "react-icons/ci";
+
 
 function AddTask() {
     return (
         <div className='my-[20px]'>
-            <button className="btn btn-info w-[300px]">Add new Task</button>
+            <button className="btn btn-info w-[300px]">Add new Task <CiCirclePlus size={20} /></button>
         </div>
     )
 }
