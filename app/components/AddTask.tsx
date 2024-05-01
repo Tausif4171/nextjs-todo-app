7facf958530a64c0f6ab87f08b10199e31e89768
Tausif Khan <tausifkhan4171@gmail.com>
2024-05-01 14:00:30 +0530

    basic form created for handling todo value

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,20 +1,35 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { FormEventHandler, useState } from 'react'
 import { CiCirclePlus } from "react-icons/ci";
 import Modal from './Modal';
 
 
 function AddTask() {
     const [showModal, setShowModal] = useState<boolean>(false);
+    const [todoValue, setTodoValue] = useState<string>('');
+
+    const handleAddTodo: React.FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault();
+        console.log(todoValue)
+        setShowModal(false)
+    }
     return (
         <div className='my-[20px]'>
             <button onClick={() => setShowModal(true)} className="btn btn-info w-[300px]">Add new Task <CiCirclePlus size={20} /></button>
 
             <Modal showModal={showModal} setShowModal={setShowModal}>
-                <div>
-                    Hii
-                </div>
+
+                <form onSubmit={handleAddTodo}>
+                    <div>
+                        <h1 className=' text-center'>Add new task</h1>
+                        <div className='flex gap-2 justify-center mt-4'>
+                            <input name={todoValue} onChange={(e) => setTodoValue(e.target.value)} type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                            <button className="btn btn-primary">Add</button>
+                        </div>
+                    </div>
+                </form>
+
             </Modal>
         </div>
     )
